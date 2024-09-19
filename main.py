3bc0a3578e653fe6ed2dd382c6b4842207f2ff1d
AustinGInmanMills <xboxboy1387@gmail.com>
2024-09-19 01:10:11 -0400

    Update main.py

--- main.py
+++ main.py
@@ -3,3 +3,7 @@ from streamlit_gsheets import GSheetsConnection
 
 st.title("Read Google Sheet as DataFrame")
 
+conn = st.connection("gsheets",type=GSheetsConnection)
+cdf = conn.read(worksheet="Machine #1")
+
+st.write(cdc)
