4449fac5389612626b504d3b51ee59bbe9269990
AustinGInmanMills <xboxboy1387@gmail.com>
2024-09-19 10:52:24 -0400

    Update main.py

--- main.py
+++ main.py
@@ -15,6 +15,7 @@ st.set_page_config(
 conn = st.connection("gsheets", type=GSheetsConnection) # Connects to gSheet conn used for connect
 machines_data = conn.read(worksheet="Knitting Machines Data", ttl=0)
 machines_data = pd.DataFrame(machines_data)
+machines_data['Machine #'] = machines_data['Machine #'].astype(str)
 
 hide_st_style = """  
             <style>
