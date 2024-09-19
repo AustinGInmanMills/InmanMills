f6e9d991b9ad186fd2b05384ee3c3d3cbc3037ca
AustinGInmanMills <xboxboy1387@gmail.com>
2024-09-19 02:59:45 -0400

    Update main.py

--- main.py
+++ main.py
@@ -7,6 +7,6 @@ st.write("Knitting Machine Database")
 conn = st.connection("gsheets",type=GSheetsConnection)
 cdf = conn.read(worksheet="Knitting Machines Data", ttl=0)
 ddd = st.dataframe(cdf)
-ddd = cdf.loc[0]
+ddd = ddd.loc[0]
 
 st.write(ddd)
