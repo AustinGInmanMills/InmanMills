5f12100b8cd23ea2d77621c8c9d779d687c884db
AustinGInmanMills <xboxboy1387@gmail.com>
2024-09-19 12:33:48 -0400

    Update main.py

--- main.py
+++ main.py
@@ -105,8 +105,10 @@ with tab2:
                         time.sleep(3)
                         error.empty()
 
-                    st.chat_message()
-                    st.chat_input()
+                    messages = st.container(height=300)
+                    if prompt := st.chat_input("Say something"):
+                        messages.chat_message("user").write(prompt)
+                        messages.chat_message("assistant").write(f"Echo: {prompt}")
 
         #############################################DOFF CALCULATOR#######################################################################################
 
