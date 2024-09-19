f3b51f208c96d381b9ad898b375f4437984f6fa1
AustinGInmanMills <xboxboy1387@gmail.com>
2024-09-19 01:24:55 -0400

    Update main.py

--- main.py
+++ main.py
@@ -2,9 +2,9 @@ import streamlit as st
 import pandas as pd
 from streamlit_gsheets import GSheetsConnection
 
-
+st.write("Knitting Machine Database")
 
 conn = st.connection("gsheets",type=GSheetsConnection)
 cdf = conn.read(worksheet="Knitting Machines Data", ttl=0)
-cdf = pd.DataFrame(cdf)
+
 st.write(cdf, hide_index=True)
