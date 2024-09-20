a8c94e53cfcca566b0a77d0610db2d3776d3ecc0
AustinGInmanMills <xboxboy1387@gmail.com>
2024-09-20 03:26:32 -0400

    Update main.py

--- main.py
+++ main.py
@@ -46,7 +46,8 @@ with placeholder.form("login"):
     st.markdown("#### Enter your credentials")
     email = st.text_input("Email")
     password = st.text_input("Password", type="password")
-    submit = st.form_submit_button("Login")
+    #submit = st.form_submit_button("Login")
+    submit = st.page_link("pages/login.py", label="Login")
 
 if submit and email == actual_email and password == actual_password:
     # If the form is submitted and the email and password are correct,
