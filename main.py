2700acce22aef6fddb0c374c757e86e9c893ac0d
AustinGInmanMills <xboxboy1387@gmail.com>
2024-09-20 02:40:57 -0400

    Update main.py

--- main.py
+++ main.py
@@ -41,6 +41,34 @@ st.markdown(
 )
 #################################################
 
+# Create an empty container
+placeholder = st.empty()
+
+actual_email = "email"
+actual_password = "password"
+
+# Insert a form in the container
+with placeholder.form("login"):
+    st.markdown("#### Enter your credentials")
+    email = st.text_input("Email")
+    password = st.text_input("Password", type="password")
+    submit = st.form_submit_button("Login")
+
+if submit and email == actual_email and password == actual_password:
+    # If the form is submitted and the email and password are correct,
+    # clear the form/container and display a success message
+    placeholder.empty()
+    st.success("Login successful")
+elif submit and email != actual_email and password != actual_password:
+    st.error("Login failed")
+else:
+    pass
+
+
+
+
+
+
 tab1, tab2, tab3 = st.tabs(["Home", "Departments", "Information"])
 
 with tab1:
@@ -61,38 +89,3 @@ with tab2:
             index=None,
             key="KnittingBox",
         )
-            
-        if knitting_bttn is None:
-            st.divider()
-            st.subheader("Doff Time Calculator")
-            calculator_machine_number = st.number_input("Machine Number ", value=None, min_value=0, max_value=21,placeholder="")
-            calculator_machine_revs = st.number_input("Current Revs", value=None, min_value=0, max_value=5000,placeholder="")
-            calculator_bttn = st.button("Calculate")
-            if calculator_bttn:
-                if calculator_machine_number is not None and calculator_machine_revs is not None:
-                    st.write("not added yet")
-        
-        if knitting_bttn == "Operator":
-            placeholder = st.empty()
-
-            actual_email = "email"
-            actual_password = "password"
-
-            # Insert a form in the container
-            with placeholder.form("login"):
-                st.markdown("#### Enter your credentials")
-                email = st.text_input("Username")
-                password = st.text_input("Password", type="password")
-                submit = st.form_submit_button("Login")
-
-            if submit and email == actual_email and password == actual_password:
-                # If the form is submitted and the email and password are correct,
-                # clear the form/container and display a success message
-                placeholder.empty()
-                successpopup = st.success("Login successful")
-                time.sleep(2)
-                successpopup.empty()
-            elif submit and email != actual_email and password != actual_password:
-                st.error("Login failed")
-            else:
-                pass
