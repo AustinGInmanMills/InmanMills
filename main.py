b2f7d489dbbf27bc085d0fdb276db00ff80ab72e
AustinGInmanMills <xboxboy1387@gmail.com>
2024-09-20 02:24:03 -0400

    Update main.py

--- main.py
+++ main.py
@@ -73,9 +73,24 @@ with tab2:
                     st.write("not added yet")
         
         if knitting_bttn == "Operator":
-            @st.dialog("Login")
-            def login_form():
-                st.caption("Username")
-                username = st.text_input()
-                st.caption("Password")
-                password = st.text_input()
+            placeholder = st.empty()
+
+            actual_email = "email"
+            actual_password = "password"
+
+            # Insert a form in the container
+            with placeholder.form("login"):
+                st.markdown("#### Enter your credentials")
+                email = st.text_input("Email")
+                password = st.text_input("Password", type="password")
+                submit = st.form_submit_button("Login")
+
+            if submit and email == actual_email and password == actual_password:
+                # If the form is submitted and the email and password are correct,
+                # clear the form/container and display a success message
+                placeholder.empty()
+                st.success("Login successful")
+            elif submit and email != actual_email and password != actual_password:
+                st.error("Login failed")
+            else:
+                pass
