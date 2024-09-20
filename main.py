f098a1231142f733aaf8cda38038f85a27603d60
AustinGInmanMills <xboxboy1387@gmail.com>
2024-09-20 03:02:02 -0400

    Update main.py

--- main.py
+++ main.py
@@ -63,29 +63,3 @@ elif submit and email != actual_email and password != actual_password:
     st.error("Login failed")
 else:
     pass
-
-
-
-
-
-
-tab1, tab2, tab3 = st.tabs(["Home", "Departments", "Information"])
-
-with tab1:
-    st.markdown(
-        "Welcome to Inman Mills Saybrook Plant new Employee website! You can view information about up coming layoffs, overtime, departments and much more")
-
-with tab2:
-    department_options_bttn = st.selectbox(
-        "Department Options",
-        ("Knitting", "Ring Spinning", "Winding", "Roven", "MJS", "MVS", "Card Room"),
-        index=None,
-        key="DepartmentBox"
-    )
-    if department_options_bttn == "Knitting":
-        knitting_bttn = st.selectbox(
-            "Knitting Department Options",
-            ("Operator", "Technician", "Overhauler", "Supervisor"),
-            index=None,
-            key="KnittingBox",
-        )
