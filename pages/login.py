45fe122744190543c5d5a13642c9825063881a23
AustinGInmanMills <xboxboy1387@gmail.com>
2024-09-20 03:03:48 -0400

    Update login.py

--- pages/login.py
+++ pages/login.py
@@ -1,4 +1,44 @@
+import time
+from operator import index
 
+import streamlit as st
+import pandas as pd
+from streamlit_gsheets import GSheetsConnection
+from math import trunc
+
+st.set_page_config(
+    page_title="Inman Mills Saybrook",
+    page_icon="",
+)
+
+hide_st_style = """
+            <style>
+            #MainMenu {visibility: hidden;}
+            footer {visibility: hidden;}
+            header {visibility: hidden;}
+            </style>
+            """
+st.markdown(hide_st_style, unsafe_allow_html=True)
+
+hide_st_style = """  
+            <style>
+            #MainMenu {visibility: hidden;}
+            footer {visibility: hidden;}
+            header {visibility: hidden;}
+            </style>
+            """  # Hides streamlit information on web app
+st.markdown(hide_st_style, unsafe_allow_html=True)
+
+st.markdown(
+    body="""
+        <style>
+            .block-container{
+                    padding-top: 25px;
+                }
+        </style>
+    """,
+    unsafe_allow_html=True  # Deletes white space on top of web app
+)
 
 
 
