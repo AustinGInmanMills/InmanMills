24758723e63f59034cbc9ff5741014ed80aa1754
AustinGInmanMills <xboxboy1387@gmail.com>
2024-09-21 06:41:22 -0400

    Update create.py

--- pages/create.py
+++ pages/create.py
@@ -1,5 +1,57 @@
+import pandas as pd
+import streamlit as st
+from streamlit import success
+from streamlit_gsheets import GSheetsConnection
+import time
 
-  streamlit. elements. empty. EmptyMixin
-@gather_metrics("empty") def empty(self) -> DeltaGenerator
- 
-Insert a single-element container.  Inserts a container into your app that can be used to hold a single element. This allows you to, for example, remove elements at any point, or replace several elements at once (using a child multi-element container).  To insert/ replace/ clear an element on the returned container, you can use ``with`` notation or just call methods directly on the returned object. See examples below.  Examples -------- Overwriting elements in-place using ``with`` notation: 
+
+conn = st.connection("gsheets", type=GSheetsConnection)
+
+
+employee_data = conn.read(worksheet="Employees Login", ttl=0)
+employee_information = conn.read(worksheet="Employees Data", ttl=0)
+employee_data = pd.DataFrame(employee_data)
+employee_information = pd.DataFrame(employee_information)
+
+
+placeholder = st.empty()
+
+
+with placeholder.form("Create"):
+    username = st.text_input("Username", key="Create Account Username")
+    password = st.text_input("Password", key="Create Account Password")
+    employee_id = st.text_input("Employee ID", key="EmployeeID")
+    registration_code = st.text_input("Registration Code", key="RegistrationCode")
+    register = st.form_submit_button("Create Account")
+    if register:
+        if registration_code == "Saybrook Plant":
+            if employee_id in employee_information.values:
+
+                if employee_id in employee_data.values:
+                    error_id = st.error("You are already in the database contact Supervisor if you forgot your login")
+                    time.sleep(3)
+                    error_id = error_id.empty()
+                else:
+                    if username in employee_data.values:
+                        error_username = st.error("Username already exist")
+                        time.sleep(3)
+                        error_username = error_username.empty()
+                    else:
+                        for x, row in employee_information.iterrows():
+                            if row["Employee ID"] == employee_id:
+                                position = row["Position"]
+                                break
+                        success = success("Account successfully created")
+                        employee_data.loc[len(employee_data.index)] = [username, password, position, employee_id]
+                        conn.update(worksheet="Employees Login", data=employee_data)
+                        time.sleep(2)
+                        success = success.empty()
+                        st.switch_page("pages/login.py")
+            else:
+                error_no_id_found = st.error("Employee ID not found")
+                time.sleep(3)
+                error_username = error_no_id_found.empty()
+        else:
+            error = st.error("Invalid Registration Code Contact Supervisor For Correct Code")
+            time.sleep(3)
+            error = error.empty()
