62de4f4057c45dca83ec11fe54f225d9a100f50e
AustinGInmanMills <xboxboy1387@gmail.com>
2024-09-21 09:35:23 -0400

    Update login.py

--- pages/login.py
+++ pages/login.py
@@ -1,12 +1,15 @@
 import pandas as pd
 import streamlit as st
-from streamlit import success
 from streamlit_gsheets import GSheetsConnection
 import time
+
+from pages.create import success
 
 conn = st.connection("gsheets", type=GSheetsConnection)  # Connects to gSheet conn used for connect
 employee_data = conn.read(worksheet="Employees Login", ttl=0)
 employee_data = pd.DataFrame(employee_data)
+employee_name = conn.read(worksheet="Employees Data", ttl=0)
+employee_name = pd.DataFrame(employee_name)
 
 placeholder = st.empty()
 
@@ -25,68 +28,78 @@ if create:
 
 
 if submit:
+    if not username in employee_data.values or not password in employee_data.values:
+        error_user_not_found = st.error("Incorrect Username/Password")
+        time.sleep(3)
+        error_user_not_found = error_user_not_found.empty()
+
     for x, row in employee_data.iterrows():
         if row['Username'] == username and row["Password"] == password:
-            if row['Position'] == "Knitting Operator":
-                st.switch_page("pages/Knitting Operator Page.py")
+            for i, rows in employee_name.iterrows():
+                if rows["Employee ID"] == row["EmployeeID"]:
+                    #if 'Name' not in st.session_state:
+                    st.session_state['Name'] = str(rows["First Name"])
 
-            elif row['Position'] == "Knitting Doffer":
-                st.switch_page('pages/Knitting Doffer Page.py')
+                    success_login = st.success("Login Successful Loading...")
+                    time.sleep(2)
+                    success_login = success_login.empty()
 
-            elif row['Position'] == "Knitting Tech":
-                st.switch_page('pages/Knitting Tech Page.py')
+                    if row['Position'] == "Knitting Operator":
+                        st.switch_page("pages/Knitting Operator Page.py")
 
-            elif row['Position'] == "Knitting Over Hauler":
-                st.switch_page('pages/Knitting Over Hauler Page.py')
+                    elif row['Position'] == "Knitting Doffer":
+                        st.switch_page('pages/Knitting Doffer Page.py')
 
-            elif row['Position'] == "Cut and Sew":
-                st.switch_page('pages/Cut and Sew Page.py')
+                    elif row['Position'] == "Knitting Tech":
+                        st.switch_page('pages/Knitting Tech Page.py')
 
-            elif row['Position'] == "Ring Spinning Operator":
-                st.switch_page('pages/Ring Spinning Operator Page.py')
+                    elif row['Position'] == "Knitting Over Hauler":
+                        st.switch_page('pages/Knitting Over Hauler Page.py')
 
-            elif row['Position'] == "Ring Spinning Tech":
-                st.switch_page('pages/Ring Spinning Tech Page.py')
+                    elif row['Position'] == "Cut and Sew":
+                        st.switch_page('pages/Cut and Sew Page.py')
 
-            elif row['Position'] == "Ring Spinning Over Hauler":
-                st.switch_page('pages/Ring Spinning Over Hauler Page.py')
+                    elif row['Position'] == "Ring Spinning Operator":
+                        st.switch_page('pages/Ring Spinning Operator Page.py')
 
-            elif row['Position'] == "Winder Operator":
-                st.switch_page('pages/Winder Operator Page.py')
+                    elif row['Position'] == "Ring Spinning Tech":
+                        st.switch_page('pages/Ring Spinning Tech Page.py')
 
-            elif row['Position'] == "Winder Tech":
-                st.switch_page('pages/Winder Tech Page.py')
+                    elif row['Position'] == "Ring Spinning Over Hauler":
+                        st.switch_page('pages/Ring Spinning Over Hauler Page.py')
 
-            elif row['Position'] == "Winder Over Hauler":
-                st.switch_page('pages/Winder Over Hauler Page.py')
+                    elif row['Position'] == "Winder Operator":
+                        st.switch_page('pages/Winder Operator Page.py')
 
-            elif row['Position'] == "MVS Operator":
-                st.switch_page('pages/MVS Operator Page.py')
+                    elif row['Position'] == "Winder Tech":
+                        st.switch_page('pages/Winder Tech Page.py')
 
-            elif row['Position'] == "MVS Tech":
-                st.switch_page('pages/MVS Tech Page.py')
+                    elif row['Position'] == "Winder Over Hauler":
+                        st.switch_page('pages/Winder Over Hauler Page.py')
 
-            elif row['Position'] == "MVS Over Hauler":
-                st.switch_page('pages/MVS Over Hauler Page.py')
+                    elif row['Position'] == "MVS Operator":
+                        st.switch_page('pages/MVS Operator Page.py')
 
-            elif row['Position'] == "MJS Operator":
-                st.switch_page('pages/MJS Operator Page.py')
+                    elif row['Position'] == "MVS Tech":
+                        st.switch_page('pages/MVS Tech Page.py')
 
-            elif row['Position'] == "MJS Tech":
-                st.switch_page('pages/MJS Tech Page.py')
+                    elif row['Position'] == "MVS Over Hauler":
+                        st.switch_page('pages/MVS Over Hauler Page.py')
 
-            elif row['Position'] == "MJS Over Hauler":
-                st.switch_page('pages/MJS Over Hauler Page.py')
+                    elif row['Position'] == "MJS Operator":
+                        st.switch_page('pages/MJS Operator Page.py')
 
-            elif row['Position'] == "Card Room Operator":
-                st.switch_page('pages/Card Room Operator Page.py')
+                    elif row['Position'] == "MJS Tech":
+                        st.switch_page('pages/MJS Tech Page.py')
 
-            elif row['Position'] == "Card Room Tech":
-                st.switch_page('pages/Card Room Tech Page.py')
+                    elif row['Position'] == "MJS Over Hauler":
+                        st.switch_page('pages/MJS Over Hauler Page.py')
 
-            elif row['Position'] == "Card Room Over Hauler":
-                st.switch_page('pages/Card Room Over Hauler Page.py')
+                    elif row['Position'] == "Card Room Operator":
+                        st.switch_page('pages/Card Room Operator Page.py')
 
-    error_login = st.error("Incorrect Username/Password")
-    time.sleep(3)
-    error_login = error_login.empty()
+                    elif row['Position'] == "Card Room Tech":
+                        st.switch_page('pages/Card Room Tech Page.py')
+
+                    elif row['Position'] == "Card Room Over Hauler":
+                        st.switch_page('pages/Card Room Over Hauler Page.py')
