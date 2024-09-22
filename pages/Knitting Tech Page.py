79fcad2ef49ebcf2fd882ee20fe92d7ff4917e4e
AustinGInmanMills <xboxboy1387@gmail.com>
2024-09-22 14:18:16 -0400

    Update Knitting Tech Page.py

--- pages/Knitting Tech Page.py
+++ pages/Knitting Tech Page.py
@@ -13,7 +13,10 @@ st.markdown(hide_st_style, unsafe_allow_html=True)
 
 if not "Name" in st.session_state:
     st.switch_page("pages/login.py")
-    
+
 conn = st.connection("gsheets", type=GSheetsConnection)
 
-st.write("Knitting Tech Page")
+tab1, tab2, tab3, tab4, tab5, tab6 = st.tabs(["Flag Sheets", "Tech Requests", "Tech Duties", "Tech Chat", "Supervisor Chat", "Schedule Information"])
+
+
+
