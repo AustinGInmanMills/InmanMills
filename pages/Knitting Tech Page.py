d057c00a6e4151f2a39ccadbcd26632516166c95
AustinGInmanMills <xboxboy1387@gmail.com>
2024-09-22 13:57:51 -0400

    Update Knitting Tech Page.py

--- pages/Knitting Tech Page.py
+++ pages/Knitting Tech Page.py
@@ -1,1 +1,19 @@
+import streamlit as st
+import pandas as pd
+from streamlit_gsheets import GSheetsConnection
 
+hide_st_style = """
+            <style>
+            #MainMenu {visibility: hidden;}
+            footer {visibility: hidden;}
+            header {visibility: hidden;}
+            </style>
+            """
+st.markdown(hide_st_style, unsafe_allow_html=True)
+
+if not "Name" in st.session_state:
+    st.switch_page("pages/login.py")
+    
+conn = st.connection("gsheets", type=GSheetsConnection)
+
+st.write("Knitting Tech Page")
