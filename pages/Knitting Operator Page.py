5379f8734cecba0f01056cd5dc90f4e73d3349ce
AustinGInmanMills <xboxboy1387@gmail.com>
2024-09-26 01:30:54 -0400

    Update Knitting Operator Page.py

--- pages/Knitting Operator Page.py
+++ pages/Knitting Operator Page.py
@@ -60,7 +60,7 @@ tab1, tab2, tab3 = st.tabs([f"Machine {machine_1}", f"Machine {machine_2}", "Sch
 with tab1:
     with st.form("Machine 1", clear_on_submit=True):
         st.write(f"Machine {machine_1} Defect Log ")
-        defect_time = st.number_input("Defect REVS", min_value=0, max_value=None, value=None,key="Defect REV")
+        #defect_time = st.number_input("Defect REVS", min_value=0, max_value=None, value=None,key="Defect REV")
         defect_type = st.selectbox(
             "Defect Type",
             ("Unknown Stop", "Needle Run", "Drop Stitch", "Hole", "Press Off", "Oil Spot", "Slub Hole", "Heavy Ends",
@@ -71,7 +71,7 @@ with tab1:
         if submit:
             machine_1_flags_data = conn.read(worksheet=f"Knitting Machine {machine_1} Flag Sheet", ttl="35s", max_entries=20)
             machine_1_flags_data = pd.DataFrame(machine_1_flags_data)
-            machine_1_flags_data.loc[len(machine_1_flags_data.index)] = [name, shift, today, current_time, "-", defect_time, defect_type]
+            machine_1_flags_data.loc[len(machine_1_flags_data.index)] = [name, shift, today, current_time, "-", defect_type]
             conn.update(worksheet=f"Knitting Machine {machine_1} Flag Sheet", data=machine_1_flags_data)
             success_defect_update_1 = st.success("Successfully Submitted")
             time.sleep(2)
@@ -93,14 +93,14 @@ with tab1:
                         error_cant_start = error_cant_start.empty()
                         break
                     else:
-                        machine_1_flags_data.loc[len(machine_1_flags_data.index)] = [name, shift, today, current_time, "Start", "-", "-"]
+                        machine_1_flags_data.loc[len(machine_1_flags_data.index)] = [name, shift, today, current_time, "Start", "-"]
                         conn.update(worksheet=f"Knitting Machine {machine_1} Flag Sheet", data=machine_1_flags_data)
                         success_new_roll_submit = st.success(f"Successfully Started New Roll On Machine {machine_1}")
                         time.sleep(2)
                         success_new_roll_submit = success_new_roll_submit.empty()
                         break
             else:
-                machine_1_flags_data.loc[len(machine_1_flags_data.index)] = [name, shift, today, current_time, "Start", "-", "-"]
+                machine_1_flags_data.loc[len(machine_1_flags_data.index)] = [name, shift, today, current_time, "Start", "-"]
                 conn.update(worksheet=f"Knitting Machine {machine_1} Flag Sheet", data=machine_1_flags_data)
                 success_new_roll_submit = st.success(f"Successfully Started New Roll On Machine {machine_1}")
                 time.sleep(2)
@@ -120,14 +120,14 @@ with tab1:
                         error_cant_start = error_cant_start.empty()
                         break
                     else:
-                        machine_1_flags_data.loc[len(machine_1_flags_data.index)] = [name, shift, today, current_time, "Doff", "-", "-"]
+                        machine_1_flags_data.loc[len(machine_1_flags_data.index)] = [name, shift, today, current_time, "Doff", "-"]
                         conn.update(worksheet=f"Knitting Machine {machine_1} Flag Sheet", data=machine_1_flags_data)
                         success_new_roll_submit = st.success(f"Successfully Doffed Roll On Machine {machine_1}")
                         time.sleep(2)
                         success_new_roll_submit = success_new_roll_submit.empty()
                         break
             else:
-                machine_1_flags_data.loc[len(machine_1_flags_data.index)] = [name, shift, today, current_time, "Doff", "-", "-"]
+                machine_1_flags_data.loc[len(machine_1_flags_data.index)] = [name, shift, today, current_time, "Doff", "-"]
                 conn.update(worksheet=f"Knitting Machine {machine_1} Flag Sheet", data=machine_1_flags_data)
                 success_new_roll_submit = st.success(f"Successfully Doffed Roll On Machine {machine_1}")
                 time.sleep(2)
@@ -136,7 +136,7 @@ with tab1:
 with tab2:
     with st.form("Machine 2", clear_on_submit=True):
         st.write(f"Machine {machine_2} Defect Log ")
-        defect_time = st.number_input("Defect REVS", min_value=0, max_value=None, value=None,key="Defect REV2")
+        #defect_time = st.number_input("Defect REVS", min_value=0, max_value=None, value=None,key="Defect REV2")
         defect_type = st.selectbox(
             "Defect Type",
             ("Unknown Stop", "Needle Run", "Drop Stitch", "Hole", "Press Off", "Oil Spot", "Slub Hole", "Heavy Ends",
@@ -147,7 +147,7 @@ with tab2:
         if submit:
             machine_2_flags_data = conn.read(worksheet=f"Knitting Machine {machine_2} Flag Sheet", ttl="35s", max_entries=20)
             machine_2_flags_data = pd.DataFrame(machine_2_flags_data)
-            machine_2_flags_data.loc[len(machine_2_flags_data.index)] = [name, shift, today, current_time, "-", defect_time, defect_type]
+            machine_2_flags_data.loc[len(machine_2_flags_data.index)] = [name, shift, today, current_time, "-", defect_type]
             conn.update(worksheet=f"Knitting Machine {machine_2} Flag Sheet", data=machine_2_flags_data)
             success_defect_update_2 = st.success("Successfully Submitted")
             time.sleep(2)
@@ -169,14 +169,14 @@ with tab2:
                         error_cant_start2 = error_cant_start2.empty()
                         break
                     else:
-                        machine_2_flags_data.loc[len(machine_2_flags_data.index)] = [name, shift, today, current_time, "Start", "-", "-"]
+                        machine_2_flags_data.loc[len(machine_2_flags_data.index)] = [name, shift, today, current_time, "Start", "-"]
                         conn.update(worksheet=f"Knitting Machine {machine_2} Flag Sheet", data=machine_2_flags_data)
                         success_new_roll_submit2 = st.success(f"Successfully Started New Roll On Machine {machine_2}")
                         time.sleep(2)
                         success_new_roll_submit2 = success_new_roll_submit2.empty()
                         break
             else:
-                machine_2_flags_data.loc[len(machine_2_flags_data.index)] = [name, shift, today, current_time, "Start", "-", "-"]
+                machine_2_flags_data.loc[len(machine_2_flags_data.index)] = [name, shift, today, current_time, "Start", "-"]
                 conn.update(worksheet=f"Knitting Machine {machine_2} Flag Sheet", data=machine_2_flags_data)
                 success_new_roll_submit2 = st.success(f"Successfully Started New Roll On Machine {machine_2}")
                 time.sleep(2)
@@ -196,14 +196,14 @@ with tab2:
                         error_cant_start2 = error_cant_start2.empty()
                         break
                     else:
-                        machine_2_flags_data.loc[len(machine_2_flags_data.index)] = [name, shift, today, current_time, "Doff", "-", "-"]
+                        machine_2_flags_data.loc[len(machine_2_flags_data.index)] = [name, shift, today, current_time, "Doff", "-"]
                         conn.update(worksheet=f"Knitting Machine {machine_2} Flag Sheet", data=machine_2_flags_data)
                         success_new_roll_submit2 = st.success(f"Successfully Doffed Roll On Machine {machine_2}")
                         time.sleep(2)
                         success_new_roll_submit2 = success_new_roll_submit2.empty()
                         break
             else:
-                machine_2_flags_data.loc[len(machine_2_flags_data.index)] = [name, shift, today, current_time, "Doff", "-", "-"]
+                machine_2_flags_data.loc[len(machine_2_flags_data.index)] = [name, shift, today, current_time, "Doff", "-"]
                 conn.update(worksheet=f"Knitting Machine {machine_2} Flag Sheet", data=machine_2_flags_data)
                 success_new_roll_submit2 = st.success(f"Successfully Doffed Roll On Machine {machine_2}")
                 time.sleep(2)
