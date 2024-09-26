36a5fc0e3a17d47b9044388e0c1653458c602753
AustinGInmanMills <xboxboy1387@gmail.com>
2024-09-26 09:09:42 -0400

    Update create.py

--- pages/create.py
+++ pages/create.py
@@ -15,12 +15,7 @@ st.markdown(hide_st_style, unsafe_allow_html=True)
 
 conn = st.connection("gsheets", type=GSheetsConnection)
 
-
-
-
-
 placeholder = st.empty()
-
 
 with placeholder.form("Create"):
     username = st.text_input("Username", key="Create Account Username")
@@ -34,33 +29,38 @@ with placeholder.form("Create"):
     if back:
         st.switch_page("pages/login.py")
     if register:
-        employee_data = conn.read(worksheet="Employees Login", ttl="120s", max_entries=800)
-        employee_name = conn.read(worksheet="Employees Data", ttl="120s", max_entries=800)
-        employee_data = pd.DataFrame(employee_data)
-        employee_name = pd.DataFrame(employee_name)
-        if employee_id in employee_name.values:
+        try:
+            employee_data = conn.read(worksheet="Employees Login", ttl="120s", max_entries=800)
+            employee_name = conn.read(worksheet="Employees Data", ttl="120s", max_entries=800)
+            employee_data = pd.DataFrame(employee_data)
+            employee_name = pd.DataFrame(employee_name)
+            if employee_id in employee_name.values:
 
-            if employee_id in employee_data.values:
-                error_id = st.error("You are already in the database contact Supervisor if you forgot your login")
-                time.sleep(2)
-                error_id = error_id.empty()
-            else:
-                if username in employee_data.values:
-                    error_username = st.error("Username already exist")
-                    time.sleep(3)
-                    error_username = error_username.empty()
-                else:
-                    for x, row in employee_name.iterrows():
-                        if row["Employee ID"] == employee_id:
-                            position = row["Position"]
-                            break
-                    success = success("Account successfully created")
-                    employee_data.loc[len(employee_data.index)] = [username, password, position, employee_id]
-                    conn.update(worksheet="Employees Login", data=employee_data)
+                if employee_id in employee_data.values:
+                    error_id = st.error("You are already in the database contact Supervisor if you forgot your login")
                     time.sleep(2)
-                    success = success.empty()
-                    st.switch_page("pages/login.py")
-        else:
-            error_no_id_found = st.error("Employee ID not found")
-            time.sleep(3)
-            error_username = error_no_id_found.empty()
+                    error_id = error_id.empty()
+                else:
+                    if username in employee_data.values:
+                        error_username = st.error("Username already exist")
+                        time.sleep(3)
+                        error_username = error_username.empty()
+                    else:
+                        for x, row in employee_name.iterrows():
+                            if row["Employee ID"] == employee_id:
+                                position = row["Position"]
+                                break
+                        success = success("Account successfully created")
+                        employee_data.loc[len(employee_data.index)] = [username, password, position, employee_id]
+                        conn.update(worksheet="Employees Login", data=employee_data)
+                        time.sleep(2)
+                        success = success.empty()
+                        st.switch_page("pages/login.py")
+            else:
+                error_no_id_found = st.error("Employee ID not found")
+                time.sleep(3)
+                error_username = error_no_id_found.empty()
+        except:
+            error_gsheet_connection = st.error("Connection to server lost reconnecting please wait")
+            time.sleep(5)
+            error_gsheet_connection.empty()
